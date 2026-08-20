6a63d9c5807ab002872f37e79b59d7060a8c7867
Ayushi Chauhan <Ayushichauhan2564@gmail.com>
2026-08-20 10:38:49 +0530

    Time: 0 ms (100%), Space: 10 MB (86.12%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,34 +1,50 @@
 class MyStack {
-    queue<int>q1;
-       queue<int>q2;
+       queue<int>q1;
+    //    queue<int>q2;
 public:
     MyStack() {
        queue<int>q1;
-       queue<int>q2; 
+    //    queue<int>q2; 
     }
     
     void push(int x) {
-    q2.push(x);
-    while(!q1.empty()){
-        q2.push(q1.front());
+    // q2.push(x);
+    // while(!q1.empty()){
+    //     q2.push(q1.front());
+    //     q1.pop();
+    // }
+    //  swap(q1,q2);   
+    // 
+    
+    q1.push(x);
+    //while(q1.front()!=x) in this condition we cant find which one is new element and which is old
+    int n=q1.size();
+    for(int i =0;i<n-1;i++)
+    {
+        q1.push(q1.front());
         q1.pop();
     }
-     swap(q1,q2);   
     }
     
-    int pop() {
-    int ans=  q1.front();
+     int pop() {
+    // int ans=  q1.front();
+    // q1.pop();
+    // return ans;
+    int ans=q1.front();
     q1.pop();
     return ans;
     }
     
     int top() {
-    return q1.front();   
+    // return q1.front(); 
+    return q1.front();  
     }
     
     bool empty() {
-      if(q1.empty())return true;
-      else return false; 
+    //   if(q1.empty())return true;
+    //   else return false; 
+    if(q1.empty())return true;
+    else return false;
     }
 };
 

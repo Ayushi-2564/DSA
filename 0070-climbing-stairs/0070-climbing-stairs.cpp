94caa57675780363a650db499f53427fd7f9145c
Ayushi Chauhan <Ayushichauhan2564@gmail.com>
2026-09-25 09:19:33 +0530

    Time: 0 ms (100%), Space: 8.5 MB (52.3%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    unordered_map<int, int>dp;
+    int fun(int i, int n){
+     if(i==n)return 1;
+      if(i>n)return 0;
+     if(dp.find(i)!=dp.end())return dp[i];
+     dp[i]=fun(i+1,n)+fun(i+2,n);
+     return dp[i];
+    }
+    int climbStairs(int n) {
+     return fun(0,n);
+    }
+};
